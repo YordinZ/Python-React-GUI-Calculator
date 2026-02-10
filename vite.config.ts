d28c3e01e7a586c2f74390f41cc68ae189fb4b1a
Yordin Herrera B. <borge.yordin@icloud.com>
2026-02-10 14:40:12 -0600

    fix: base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/PYTHON-REACT-GUI-CALCULATOR/",
+  base: "/Python-React-GUI-Calculator/",
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
